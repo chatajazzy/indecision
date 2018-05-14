f10e602ccd413f11c7a71e24eb985cecc6cdcc9f
Unknown <chatajazzy@interia.pl>
2018-05-14 19:47:59 +0200

    Add options list

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,8 +4,17 @@ import Option from './Option';
 const Options = props => {
   return (
     <div>
-      <button>Remove All</button>
-      options
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && (
+        <p>Please add an option to get started!</p>
+      )}
+      {props.options.map(option => (
+        <Option
+          key={option}
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))}
     </div>
   );
 };
