f00dbaf82f87606ed6aa88706a6ed8b377251d3f
Unknown <chatajazzy@interia.pl>
2018-05-14 19:48:25 +0200

    Add adding new option component

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class AddOption extends Component {
+export default class AddOption extends React.Component {
+  state = {
+    error: undefined
+  };
+  handleAddOption = e => {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
+  };
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
           <input type="text" name="option" />
           <button>Add Option</button>
         </form>
